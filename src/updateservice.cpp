41f334dcbfc37a4c4ea47014798aca34e0bd1dc4
Adam Fidel <adam@fidel.id>
2025-01-11 23:05:07 -0600

    querying systemd through dbus now working

--- src/updateservice.cpp
+++ src/updateservice.cpp
@@ -14,42 +14,37 @@ QString kUpdateServiceName = "ublue-update.timer"_L1;
 
 bool UpdateService::isEnabled()
 {
-  QDBusMessage message = QDBusMessage::createMethodCall(
+  QDBusMessage getUnitMessage = QDBusMessage::createMethodCall(
     "org.freedesktop.systemd1"_L1,
     "/org/freedesktop/systemd1"_L1,
     "org.freedesktop.systemd1.Manager"_L1,
     "GetUnit"_L1
   );
 
-  message.setArguments({kUpdateServiceName});
-  QDBusMessage reply = QDBusConnection::systemBus().call(message);
+  getUnitMessage.setArguments({kUpdateServiceName});
+  QDBusMessage getUnitReply = QDBusConnection::systemBus().call(getUnitMessage);
 
-  qDebug() << message;
+  qDebug() << getUnitMessage;
+  qDebug() << getUnitReply;
 
-  qDebug() << reply;
+  QDBusObjectPath objectPath = getUnitReply.arguments().at(0).value<QDBusObjectPath>();
 
-  QDBusObjectPath objectPath = reply.arguments().at(0).value<QDBusObjectPath>();
-
-  QDBusInterface iface( "org.freedesktop.systemd1"_L1,
-                        objectPath.path(),
-                        "org.freedesktop.systemd1.Unit"_L1,
-                        QDBusConnection::systemBus());
-  QDBusMessage activeStateReply = iface.call("Get"_L1);
-
-  /*QDBusMessage activeStateMessage = QDBusMessage::createMethodCall(
+  QDBusMessage activeStateMessage = QDBusMessage::createMethodCall(
     "org.freedesktop.systemd1"_L1,
     objectPath.path(),
-    "org.freedesktop.systemd1.Unit"_L1,
-    "ActiveState"_L1
-  );*/
-  //qDebug() << activeStateMessage;
+    "org.freedesktop.DBus.Properties"_L1,
+    "Get"_L1
+  );
 
-  //QDBusMessage activeStateReply = QDBusConnection::systemBus().call(activeStateMessage);
+  activeStateMessage.setArguments({"org.freedesktop.systemd1.Unit"_L1, "ActiveState"_L1});
+  QDBusMessage activeStateReply = QDBusConnection::systemBus().call(activeStateMessage);
 
+  qDebug() << activeStateMessage;
   qDebug() << activeStateReply;
 
-  //std::cout << "systemd canconvert3 " << reply.arguments().at(0).canConvert<QDBusVariant>() << std::endl;
+  QString activeState = activeStateReply.arguments().at(0).value<QDBusVariant>().variant().value<QString>();
 
+  qDebug() << activeState;
 
-  return true;
+  return activeState == "active"_L1;
 }
